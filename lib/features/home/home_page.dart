276e08ad33c26ab596ee6ecd7621613b5260ab25
whatever413y <angel.tardaguela@gmail.com>
2025-04-15 14:27:21 +0800

    Update home_page.dart

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'billing_page.dart';
-import 'history_page.dart';
+import '../billing/billing_page.dart';
+import '../history/history_page.dart';
 
 class HomePage extends StatefulWidget {
   final String inputText;
@@ -13,41 +13,58 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   void _navigateToPage(Widget page) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => page),
-    );
+    Navigator.push(context, MaterialPageRoute(builder: (context) => page));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          widget.inputText,
-          style: TextStyle(fontWeight: FontWeight.bold),
+    return Scaffold(appBar: _buildAppBar(context), body: _buildBody(context));
+  }
+
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(
+      title: Text(
+        "Home",
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 24,
+          color: Colors.white,
         ),
-        backgroundColor: Colors.blue.shade800,
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.blue.shade900, Colors.blue.shade500],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
+      backgroundColor: Colors.blue.shade900,
+      centerTitle: true,
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back, color: Colors.white),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.blue.shade900, Colors.blue.shade500],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
         ),
-        child: Center(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                _buildSquareButton("Current Bill", Icons.receipt, BillingPage()),
-                SizedBox(height: 20),
-                _buildSquareButton("Billing History", Icons.history, HistoryPage()),
-              ],
-            ),
+      ),
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _buildSquareButton("Current Bill", Icons.receipt, BillingPage()),
+              SizedBox(height: 20),
+              _buildSquareButton(
+                "Billing History",
+                Icons.history,
+                HistoryPage(),
+              ),
+            ],
           ),
         ),
       ),
